0c79ff1f622959f669975cc859498d30a31a90dd
Harsh Singla <harshsingla@Harshs-MacBook-Pro.local>
2021-09-24 23:31:54 -0400

    Hoisting

---
+++ hoisting.js
@@ -0,0 +1,10 @@
+`use strict`;
+
+//Question : What is hoisting?
+// Answer : Hoisting in javascript refer to the concept of using variables/functions before their declaration.
+
+
+//var variables can be hoisted
+console.log(a); // it will undefined
+
+var a = 10;
